15dcbaf49f32cdbfb31e35f13821a458307b77ac
shihuan <shihuan@shanzhen.com>
2021-08-12 14:42:18 +0800

    ### 判断子序列

---
+++ src/main/java/attempted/IsMatch.java
@@ -0,0 +1,107 @@
+package attempted;
+
+import java.util.LinkedList;
+
+/**
+ * [10] 正则表达式匹配
+ * https://leetcode-cn.com/problems/regular-expression-matching/
+ */
+public class IsMatch {
+
+    public static void main(String[] args) {
+//        System.out.println(new IsMatch().isMatch("aa", "a"));
+//        System.out.println(new IsMatch().isMatch("aa", "a*"));
+//        System.out.println(new IsMatch().isMatch("ab", ".*"));
+//        System.out.println(new IsMatch().isMatch("aab", "c*a*b"));
+//        System.out.println(new IsMatch().isMatch("mississippi", "mis*is*p*."));
+//        System.out.println(new IsMatch().isMatch("mississippi", "mis*is*ip*."));
+//        System.out.println(new IsMatch().isMatch("aaa", "ab*a*c*a"));
+//        System.out.println(new IsMatch().isMatch("aaa", ".*"));
+//        System.out.println(new IsMatch().isMatch("baaa", "b.*"));
+        System.out.println(new IsMatch().isMatch("baaaa", "ba.*"));
+//        System.out.println(new IsMatch().isMatch("aaaa", "ab*a*c*a"));
+    }
+
+    public boolean isMatch(String s, String p) {
+        char[] sArr = s.toCharArray();
+        LinkedList<Character> contentList = new LinkedList<>();
+        for (char content : sArr) {
+            contentList.addFirst(content);
+        }
+
+        char[] pArr = p.toCharArray();
+        LinkedList<Character> parseList = new LinkedList<>();
+        for (char parse : pArr) {
+            parseList.addFirst(parse);
+        }
+
+        Character lastValidChar = null;
+        while (!parseList.isEmpty()) {
+            Character parseContent = parseList.removeFirst();
+            switch (parseContent) {
+                case '.':
+                    if (contentList.isEmpty()) {
+                        return false;
+                    }
+                    contentList.removeFirst();
+                    break;
+                case '*':
+                    Character parse = parseList.removeFirst();
+                    if (parse == '.') {
+                        if (!contentList.isEmpty()) {
+                            contentList.removeFirst();
+                        }
+                        Character content = null;
+                        while (!contentList.isEmpty()) {
+                            if (content == null) {
+                                content = contentList.removeFirst();
+                                lastValidChar = content;
+                                continue;
+                            }
+                            if (contentList.getFirst() == content) {
+                                contentList.removeFirst();
+                            } else {
+                                break;
+                            }
+                        }
+                    } else {
+                        while (!contentList.isEmpty()) {
+                            if (contentList.getFirst() == parse) {
+                                contentList.removeFirst();
+                                lastValidChar = parse;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    if (contentList.isEmpty()) {
+                        if (parseContent == lastValidChar) {
+                            break;
+                        } else {
+                            return false;
+                        }
+                    }
+                    Character content = contentList.getFirst();
+                    if (parseContent != content) {
+                        if (parseContent != lastValidChar) {
+                            return false;
+                        } else {
+                            contentList.removeFirst();
+                        }
+                    }
+                    break;
+            }
+        }
+
+        if (!contentList.isEmpty()) {
+            for (Character character : contentList) {
+                if (character != lastValidChar) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
--- src/main/java/solved/MaxProfit.java
+++ src/main/java/solved/MaxProfit.java
@@ -10,6 +10,11 @@ public class MaxProfit {
         System.out.println(new MaxProfit().maxProfit(new int[]{7, 6, 4, 3, 1}));
     }
 
+
+    /**
+     * 贪心算法
+     * 取左最小值 取右边最大值
+     */
     public int maxProfit(int[] prices) {
         int low = prices[0];
         int profit = 0;
@@ -18,5 +23,26 @@ public class MaxProfit {
             profit = Math.max(prices[i] - low, profit);
         }
         return profit;
+    }
+
+    /**
+     * 暴力破解
+     */
+    public int maxProfit_(int[] prices) {
+        int profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                profit = Math.max(profit, prices[j] - prices[i]);
+            }
+        }
+        return profit;
+    }
+
+    /**
+     * 动态规划
+     */
+    public int maxProfit__(int[] prices) {
+        // TODO
+        return 1;
     }
 }
