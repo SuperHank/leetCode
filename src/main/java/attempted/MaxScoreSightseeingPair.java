1db3d5e6033ad669555fede0671c2aba648bf224
shihuan <shihuan@shanzhen.com>
2021-08-13 19:30:47 +0800

    中等-两数相加

---
+++ src/main/java/attempted/MaxScoreSightseeingPair.java
@@ -0,0 +1,50 @@
+package attempted;
+
+/**
+ * [1014] 最佳观光组合
+ * https://leetcode-cn.com/problems/best-sightseeing-pair/
+ */
+public class MaxScoreSightseeingPair {
+
+    public static void main(String[] args) {
+        System.out.println(new MaxScoreSightseeingPair().maxScoreSightseeingPair(new int[]{8, 1, 5, 2, 6}));
+        System.out.println(new MaxScoreSightseeingPair().maxScoreSightseeingPair(new int[]{1, 2}));
+    }
+
+    /**
+     * 暴力破解
+     */
+    public int maxScoreSightseeingPair(int[] values) {
+        if (values.length < 2) {
+            return 0;
+        }
+
+        int maxValue = 0;
+        for (int i = 0; i < values.length; i++) {
+            for (int j = i + 1; j < values.length; j++) {
+                maxValue = Math.max(values[j] + values[i] + i - j, maxValue);
+            }
+        }
+
+        return maxValue;
+    }
+
+    /**
+     * TODO
+     * 动态规划
+     */
+    public int maxScoreSightseeingPair_(int[] values) {
+        if (values.length < 2) {
+            return 0;
+        }
+
+        int dp[] = new int[values.length];
+
+        dp[0] = 0;
+        for (int i = 1; i < values.length; i++) {
+            dp[i] = Math.max(dp[i - 1], values[i] + values[i - 1] + 1);
+        }
+
+        return dp[values.length - 1];
+    }
+}
