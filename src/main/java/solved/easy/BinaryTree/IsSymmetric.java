095aeca44870bbad7d44657b81511da4685b58dc
shihuan <shihuan@shanzhen.com>
2021-09-18 16:47:19 +0800

    简单-对称二叉树-广度优先

--- src/main/java/solved/easy/BinaryTree/IsSymmetric.java
+++ src/main/java/solved/easy/BinaryTree/IsSymmetric.java
@@ -1,5 +1,7 @@
 package solved.easy.BinaryTree;
 
+import java.util.LinkedList;
+
 /**
  * [100] 相同的树
  * https://leetcode-cn.com/problems/same-tree/
@@ -8,9 +10,9 @@ public class IsSymmetric {
 
     public static void main(String[] args) {
         TreeNode root1 = new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)), new TreeNode(2, new TreeNode(4), new TreeNode(3)));
-        System.out.println(new IsSymmetric().isSymmetric(root1));
+        System.out.println(new IsSymmetric().isSymmetric_bfs(root1));
         TreeNode root3 = new TreeNode(1, new TreeNode(2, null, new TreeNode(3)), new TreeNode(2, null, new TreeNode(3)));
-        System.out.println(new IsSymmetric().isSymmetric(root3));
+        System.out.println(new IsSymmetric().isSymmetric_bfs(root3));
     }
 
     /**
@@ -34,11 +36,36 @@ public class IsSymmetric {
         return compare(left.left, right.right) && compare(left.right, right.left);
     }
 
-
-    public boolean isSymmetric_iter(TreeNode root) {
+    /**
+     * 广度优先
+     */
+    public boolean isSymmetric_bfs(TreeNode root) {
         if (root == null) {
             return true;
         }
-        return compare(root.left, root.right);
+
+        LinkedList<TreeNode> queue = new LinkedList<>();
+
+        queue.addLast(root.left);
+        queue.addLast(root.right);
+
+        while (!queue.isEmpty()) {
+            TreeNode left = queue.removeFirst();
+            TreeNode right = queue.removeFirst();
+
+            if (left == null && right == null) {
+                continue;
+            } else if (left == null || right == null) {
+                return false;
+            } else if (left.val != right.val) {
+                return false;
+            }
+
+            queue.addLast(left.left);
+            queue.addLast(right.right);
+            queue.addLast(left.right);
+            queue.addLast(right.left);
+        }
+        return true;
     }
 }
